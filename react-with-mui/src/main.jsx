6ef2e2faed8485da2b71d9fa430662af3c1e34e2
carlos260800 <carlos260800@gmail.com>
2022-09-22 13:31:44 -0500

    clase 21/09/22

---
+++ react-with-mui/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Login, Dashboard} from './components'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login/>
+    <Dashboard />
+  </React.StrictMode>
+)
